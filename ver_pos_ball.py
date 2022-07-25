e2189a166a9ced7de092af38456251b42bb43fb2
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-07-26 00:10:24 +0530

    Update ver_pos_ball.py

--- ver_pos_ball.py
+++ ver_pos_ball.py
@@ -5,3 +5,11 @@ Time = 0.6
 VerticalPositionOfBall = intial_velocity*Time - 0.5*acceleration_of_gravity*Time**2
 
 print(VerticalPositionOfBall)
+
+
+v0 = 5 
+g = 9.81
+t = 0.6
+y = v0*t - 0.5*g*t**2
+
+print(y)
